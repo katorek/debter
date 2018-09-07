e2ec6ec8c65e99950b7040340182e48dc7766546
Wojciech Jaroński <wojtasj1996@gmail.com>
2018-09-07 20:02:03 +0200

    Initial commit

---
+++ src/test/java/com/wjaronski/debter/manager/DebtManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wjaronski.debter.manager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DebtManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
